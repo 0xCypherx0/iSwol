3c8add148dd1748e45ba7350536abbabc3252a5b
Montrell Jubilee <mjubil1@students.towson.edu>
2017-12-12 20:09:58 -0500

    Updated the register form

--- about/about.php
+++ about/about.php
@@ -5,36 +5,58 @@
 <link rel="stylesheet" href="style.css" type="text/css">
 
 <html>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="../COSC412-Fall-2017-Group-5-agonza7webPage/iSwol/UserHomePage/UserHomePage.php">Home</a></li>
-                
-                <li><a href="#">About</a></li>
-
-                <li><a href="../game/iSwol-gamebranch/iSwol-gamebranch/game.php">Game</a></li>
-
-                <li><a href="../contactUs/form.php">Contact Us</a></li>
-
-                <?php if (isset($_SESSION['success'])): ?>
-            <div class="error success">
-                <h3>
-                    <?php
-                        echo $_SESSION['success'];
-                        unset($_SESSION['success']);
-                    ?>
-                </h3>
-            </div>
-            <?php endif ?>
-
-            <?php if(isset($_SESSION["username"])): ?>
-                <li><a href="../login/login.php">Logout</a></li>
-            <?php endif ?>
-            </ul>
-        </nav>  
-    </header>
 
     <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="../COSC412-Fall-2017-Group-5-agonza7webPage/iSwol/UserHomePage/UserHomePage.php">Home</a></li>
+                    
+                    <li><a href="#">About</a></li>
 
+                    <li><a href="../game/iSwol-gamebranch/iSwol-gamebranch/game.php">Game</a></li>
+
+                    <li><a href="../contactUs/form.php">Contact Us</a></li>
+
+                    <?php if (isset($_SESSION['success'])): ?>
+                <div class="error success">
+                    <h3>
+                        <?php
+                            echo $_SESSION['success'];
+                            unset($_SESSION['success']);
+                        ?>
+                    </h3>
+                </div>
+                <?php endif ?>
+
+                <?php if(isset($_SESSION["username"])): ?>
+                    <li><a href="../login/login.php">Logout</a></li>
+                <?php endif ?>
+                </ul>
+            </nav>  
+        </header>
+
+        <div>
+            <h1 style="text-align:center;">About Us</h1>
+        </div>
+
+        <div>
+            <p style="text-align:center; margin-top: 10px;">  A recent study led by the U.S. Department of Education, in coordination with representatives from each state’s board of education and a team of world-renowned health, fitness and nutrition professionals, has revealed that today’s teen athletes and their parents/guardians are greatly lacking in sufficient knowledge to ensure that the young athlete has the proper nutrition and fitness routine to support the increasingly rigorous training required by high school athletic programs. As a result, these young athletes are not optimizing their potential on the sports fields through proper off-season training as well as developing healthy nutritional habits. Some athletes even experience health issues and injuries!
+Congress acknowledged the importance of youth athletic programs for a vast multitude of reasons and, as a result of this study, made the decision to allocate a generous budget to the DOE to further explore a solution to the issue.  They have a team of individuals from the original study in charge of the effort.  This team of experts (aka Subject Matter Experts or SMEs) has decided that engaging today’s teens in some type of subscription-based online gaming system would be the ideal solution to the problem.  They are currently soliciting proposals from reputable web development companies throughout the country for potential solutions.  This team fully admits that while they are experts in their respective fields that they are lacking when it comes to the latest technology, and they have stressed how vital it is that the solution be easy to use.  
+The general purpose of the website would be to create a fun, interactive learning environment for teens to become more aware of what they should be doing to maximize their performance in a given sport.  
+ </p>
+        </div>
+
+        <div style="margin-top: 20px;">
+
+            <div style="display:inline;">
+                <img src = "../COSC412-Fall-2017-Group-5-agonza7webPage/iSwol/spartan.png" align="left"/>
+            </div>
+
+            <div style="display:inline;">
+                <h3>Welcome fellow athlete's</h3>
+                <p class="para2">Spartan Technologies is currently a start up Mobile Application Developing company founded by Towson University Students Montrell Jubilee, David Hanlon, and Jared Conn on March 31st, 2017. Here at SparTek, our primary mission is to create simplistic, efficient, and graphically appealing mobile  applications for our customers to utilize.</p>
+            </div>
+        </div>
     </body>
 </html>
--- register/register.php
+++ register/register.php
@@ -6,103 +6,53 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         
 		<!-- Website CSS style -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-
-		<!-- Website Font style -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-		<link rel="stylesheet" href="style.css">
-		<!-- Google Fonts -->
-		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
-		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
-
+        <link rel="stylesheet" href="style.css">
+	
 		<title>Register</title>
 	</head>
-	<body style="background: url(../images/background.jpg); background-size: 100% 100%;" class="cl_white text-center">	
+	<body>
+		<h5 style="text-align: center; font-size: 22px; color: #fff;">Register below</h5>
+	
 		<div class="container">
-			<div class="row main">
-				<div class="main-login main-center">
-				<h5 style="text-align: center; font-size: 22px; color: grey;">Register below</h5>
-					<form class="myForm" method="post" action="register.php">
-                    <!-- display validation errors here -->
-                    <?php include('../errors.php');?>
-                        <div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>First Name</label>
-									<input type="text" class="form-control" name="fname" id="name"  placeholder="First Name"/>
-								</div>
-							</div>
-						</div>
- 
-						<div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>Last Name</label>
-									<input type="text" class="form-control" name="lname" id="name"  placeholder="Last Name"/>
-								</div>
-							</div>
-						</div>
+			<form  method="post" action="register.php">
+				<!-- display validation errors here -->
+				<?php include('../errors.php');?>
+					<div class="form-input">
+						<input type="text" name="fname" id="name"  placeholder="First Name"/>
+					</div>
 
-						<div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>Username</label>
-									<input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
-								</div>
-							</div>
-						</div>
+					<div class="form-input">
+						<input type="text" name="lname" id="name"  placeholder="Last Name"/>
+					</div>
 
-						<div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>Email</label>
-									<input type="text" class="form-control" name="email" placeholder="Enter your Email"/>
-								</div>
-							</div>
-						</div>
+					<div class="form-input">
+						<input type="text" name="username" id="username"  placeholder="Enter your Username"/>
+					</div>
 
-						<div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>Password</label>
-									<input type="password" class="form-control" name="password_1" id="password"  placeholder="Enter your Password"/>
-								</div>
-							</div>
-						</div>
+					<div class="form-input">
+						<input type="text" name="email" placeholder="Enter your Email"/>
+					</div>
 
-						<div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>Confirm Password</label>
-									<input type="password" class="form-control" name="password_2" id="confirm"  placeholder="Confirm your Password"/>
-								</div>
-							</div>
+					<div class="form-input">
+						<input type="password" name="password_1" id="password"  placeholder="Enter your Password"/>
+					</div>
+					<div class="form-input">
+						<input type="password" name="password_2" id="confirm"  placeholder="Confirm your Password"/>
+					</div>
 
-						<div class="form-group">
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<label>Activation Key</label>
-									<input type="text" class="form-control" name="actKey" id="password"  placeholder="Activation Key"/>
-								</div>
-							</div>
-						</div>
-
-							<div>
-								<button class = "regBtn" type="submit" name="register">Register</button>
-                            </div>
-                            
-                            <p>
-							    Already a member? <a href="../login/login.php">Sign in</a>
-						    </p>
-						</div>
-					</form>
+					<div class="form-input">
+						<input type="text" name="actKey" id="password"  placeholder="Activation Key"/>
+					</div>
+			
+					<div>
+						<button class="regBtn" type="submit" name="register">Register</button>
+					</div>
+					
+					<p>
+						Already a member? <a href="../login/login.php">Sign in</a>
+					</p>
 				</div>
-			</div>
+			</form>
 		</div>
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
